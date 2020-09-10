c031a20eaf6d08ad773fbe4c0e0605d22c521101
Trevor <trevor@osrs-stat.com>
2020-09-10 10:33:36 +0900

    Revert initializer module in order to not contaminate nested module namespace

--- lib/msh/scaffold.rb
+++ lib/msh/scaffold.rb
@@ -3,7 +3,7 @@ require_relative "validation"
 module Msh
   # Quick way to start including hash validation. May be the only module some projects need :)
   module Scaffold
-    prepend Msh::Validation
-    prepend Msh::Validation::Default
+    # prepend Msh::Validation
+    # prepend Msh::Validation::Default
   end
 end
