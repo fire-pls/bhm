08cc39eee9b1b7c75a2330713c76154ac9f09075
Trevor James <trevor@osrs-stat.com>
2020-09-12 12:06:31 +0900

    Fix explicit key+typecheck validator handling

--- lib/msh/validation.rb
+++ lib/msh/validation.rb
@@ -4,108 +4,112 @@ require_relative "utils"
 module Msh
   # The meat of the library
   module Validation
-    # NOTE: Methods **MUST** be in the included block -- otherwise we pollute the module constants with
-    # any "extended initializer"
     def self.included(receiver)
-      # Minimal config for key case validation + hash key types.
-      # Supplying no config will not validate hash keys!
-      # Ie, the vaildators must map 1:1 with the hash key value:
-      #
-      #   validator :foo, ->(v){}
-      #
-      # Will look for :foo (symbol). Whereas:
-      #
-      #   config { keys: :strings }
-      #   validator :foo, ->(v){}
-      #
-      # Will look for a key "foo" (string)
-      receiver.singleton_class.define_method(:config) do |**cfg|
-        pending_case, pending_keys = cfg.values_at(:casing, :keys)
-        receiver.instance_variable_set(:@___assert_case, pending_case)
-        receiver.instance_variable_set(:@___assert_keys, pending_keys)
+      # NOTE: Methods **MUST** be in the included block -- otherwise we pollute the module constants with
+      # any "initializer" module we include/extend
+      receiver.singleton_class.class_eval do
+        def config(**cfg)
+          pending_case, pending_keys = cfg.values_at(:default_key_case, :default_key_type)
 
-        case_options = [:lower_snake, :lowerCamel, :UpperCamel, :SCREAMING_SNAKE, nil]
-        fail ArgumentError, "casing must be one of #{case_options.inspect}" unless case_options.include? pending_case
+          case_options = [:lower_snake, :lowerCamel, :UpperCamel, :SCREAMING_SNAKE, nil]
+          fail ArgumentError, "casing must be one of #{case_options.inspect}" unless case_options.include? pending_case
+          @___default_key_case = pending_case
 
-        key_options = [:symbols, :strings, nil]
-        fail ArgumentError, "keys must be one of #{key_options.inspect}" unless key_options.include? pending_keys
-      end
+          key_options = [:symbols, :strings, nil]
+          fail ArgumentError, "keys must be one of #{key_options.inspect}" unless key_options.include? pending_keys
+          @___default_key_type = pending_keys
+        end
 
-      # Concise API for setting both case assertion & key conformity
-      receiver.singleton_class.define_method(:keys) do |arg|
-        key_option = arg.is_a?(String) ? :strings : :symbols
-        receiver.config(
-          keys: key_option,
-          casing: arg.to_sym
-        )
-      end
+        # Concise API for setting both case assertion & key conformity simultaneously
+        def keys(arg)
+          key_option = arg.is_a?(String) ? :strings : :symbols
+          config(
+            default_key_type: key_option,
+            default_key_case: arg.to_sym
+          )
+        end
 
-      # TODO: `nullable` body method
+        def default_key_case
+          # @___default_key_case ||= TODO_GLOBAL_CONFIG
+          @___default_key_case
+        end
 
-      receiver.singleton_class.define_method(:guard) do |handler|
-        grds = receiver.instance_variable_get(:@___guards) || []
-        grds << handler
-        receiver.instance_variable_set(:@___guards, grds)
-      end
+        def default_key_type
+          # @___default_key_type ||= TODO_GLOBAL_CONFIG
+          @___default_key_type
+        end
 
-      receiver.singleton_class.define_method(:guards) do
-        receiver.instance_variable_get(:@___guards) || []
-      end
+        # TODO: `nullable` method
 
-      receiver.singleton_class.define_method(:validators) do
-        receiver.instance_variables.map do |ivar|
-          next if ivar.to_s.start_with? "@___"
+        def guard(handler)
+          guards << handler
+        end
 
-          base_key = ivar.to_s.gsub(/^@/, "")
-          # TODO: Global default config here
-          formatted_key = case receiver.assert_keys
-          when :strings then base_key
-          else
-            base_key.to_sym # Default to symbol keys
+        def guards
+          @___guards ||= []
+        end
+
+        #### 4 ways to define a validator:
+        ## Explicit key+proc:
+        # validator "KEY", ->(){}
+        # @KEY = "KEY", ->(){}
+        #
+        ## Explicit key+typecheck
+        # validator "KEY", String
+        # @KEY = "KEY", String
+        #
+        ## Implicit proc
+        # @___default_key_type = :strings
+        # @KEY = ->(){}
+        #
+        ## Implicit typecheck
+        # @___default_key_type = :strings
+        # @KEY = String
+        def validators
+          instance_variables.each_with_object({}) do |ivar, out|
+            # Skip our internals
+            next if ivar.to_s.start_with? "@___"
+
+            arg = instance_variable_get(ivar)
+            final_key, ambiguous_arg = case arg
+            when Array
+              # Assume the array idx0 is the key, idx1 is the handler or a Ruby class
+              arg.slice(0, 2)
+            else
+              # Assume some implicit flow. Use ivar name as key value
+              subbed = ivar.to_s.gsub(/^@/, "")
+              formatted_key = case default_key_type
+              when :strings then subbed
+              when :symbols then subbed.to_sym
+              else
+                # In the event no default settings are provided, default to sym assertion
+                subbed.to_sym
+              end
+
+              # Re-send the original arg
+              [formatted_key, arg]
+            end
+
+            # Lastly, check the ambiguous arg & set the finalized handler
+            out[final_key] = case ambiguous_arg
+            when Proc then ambiguous_arg # It is a dedicated proc handler
+            when Class then ->(fetched_value) { fetched_value.is_a?(ambiguous_arg) } # They want us to typecheck
+            else
+              # Assume bad user input
+              fail ArgumentError, "could not implement validator (supplied arg: #{ambiguous_arg.inspect}"
+            end
           end
+        end
 
-          # TODO: check if handler is lambda or class.
-          ## If provided arg is simply a proc/lambda, use it as the validator.
-          arg = receiver.instance_variable_get(ivar)
-          handler = if arg.is_a?(Proc)
-            arg
-          else
-            # Else, assume we are performing a simple typecheck.
-            # eg:
-            # @name = String
-            ### val = hash.fetch("name")
-            ### val.is_a? String
-
-            # TODO: Double check default handler logic here.
-            ->(fetched_value) { fetched_value.is_a?(arg) }
-          end
-
-          # Hash signature:
-          # key => sym or string
-          # handler => proc/lambda running the check on the key
-          [formatted_key, handler]
-        end.compact.to_h
-      end
-
-      receiver.singleton_class.define_method(:assert_case) do
-        receiver.instance_variable_get(:@___assert_case)
-      end
-
-      receiver.singleton_class.define_method(:assert_keys) do
-        receiver.instance_variable_get(:@___assert_keys)
-      end
-
-      # Make getters go through config
-      receiver.singleton_class.define_method(:assert_case=) do |new_case|
-        receiver.config(keys: assert_keys, casing: new_case)
-      end
-
-      receiver.singleton_class.define_method(:assert_keys=) do |new_type|
-        receiver.config(keys: new_type, casing: assert_case)
+        # Explicit API to set a key & handler, ignoring case/key-type config
+        def validator(key, handler)
+          # Use our array handling, to specifically bypass key checks
+          instance_variable_set("@#{key}", [key, handler])
+        end
       end
 
       # Define getters for each of these methods
-      %i[validators guards assert_case assert_keys].each do |method_name|
+      %i[validators guards default_key_case default_key_type].each do |method_name|
         define_method(method_name) do
           singleton_class.included_modules.find { |mod|
             mod.include? Validation
@@ -116,22 +120,18 @@ module Msh
 
     # TODO: Assert the keys hash conform to a casing stantard
     def validate_keys!
-      if assert_keys
-        klass = case assert_keys
+      if default_key_type
+        klass = case default_key_type
         when :symbols then Symbol
         when :strings then String
         end
 
-        p "_____________"
-        p klass
-
         keys.each do |string_or_sym|
-          p string_or_sym
-          Errors::InvalidKeyType.raise!(receiver: self, key: string_or_sym, type: assert_keys) unless string_or_sym.is_a? klass
+          Errors::InvalidKeyType.raise!(receiver: self, key: string_or_sym, type: default_key_type) unless string_or_sym.is_a? klass
         end
       end
 
-      case assert_case
+      case default_key_case
       when :lower_snake
         # TODO
       when :lowerCamel
